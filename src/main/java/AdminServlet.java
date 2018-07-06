12fcaf65fa36b867c3c3de5de9635e8fffa9612a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-07-06 20:18:50 +0300

    v2

--- src/main/java/AdminServlet.java
+++ src/main/java/AdminServlet.java
@@ -12,6 +12,9 @@ public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie[] cookies = req.getCookies();
+        for (Cookie c : cookies) {
+            c.g
+        }
 
 
         PrintWriter w = resp.getWriter();
